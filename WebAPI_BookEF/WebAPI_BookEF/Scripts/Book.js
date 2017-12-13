ea099fc1354c2d38be883503c4f42306f71ac7f5
Ben Spark <bhrugen1990@gmail.com>
2017-12-12 20:07:59 -0600

    Edit Button and HTTP GET Web API

--- WebAPI_BookEF/WebAPI_BookEF/Scripts/Book.js
+++ WebAPI_BookEF/WebAPI_BookEF/Scripts/Book.js
@@ -26,6 +26,18 @@ function deleteBook(id) {
     });
 }
 
+function editBook(id) {
+    $("#txtNewBook").hide();
+    $("#btnAddBook").hide();
+    drawItemsEdit(id);
+}
+
+function cancelEditMode() {
+    $("#txtNewBook").show();
+    $("#btnAddBook").show();
+    getBookAndDrawItems();
+}
+
 function drawItems(result) {
 
     var $list = $("#bookList").empty();
@@ -43,9 +55,34 @@ function drawItems(result) {
     }
 }
 
+function drawItemsEdit(id) {
+    var $list = $("#bookList").empty();
+    var $div = $("<div class='row'>");
+    var $textBoxDiv = $("<div class='col-sm-8' style='padding-left:30px;'>").appendTo($div);
+    var $buttonDiv = $("<div class='col-sm-4' style='padding-right:30px;'>").appendTo($div);
+    var $txtBook = $("<input type='text' class='form-control' id='txtUpdate_" + id + "' value ='" + name + "'/>").appendTo($textBoxDiv);
+    var $editBtn = $("<button class='btn btn-success' style='width:50%' onclick='updateBook( " + id + ")'>Update</button>").appendTo($buttonDiv);
+    var $deleteBtn = $("<button class='btn btn-danger' style='width:50%' onclick='cancelEditMode()'>Cancel</button>").appendTo($buttonDiv);
+
+    $div.appendTo($list);
+
+    $.ajax({
+        type: "GET",
+        dataType: "json",
+        url: "api/Book/" + id,
+        success: function (result) {
+            $("#txtUpdate_" + id).val(result.Name);
+
+        }
+    });
+}
 
 $(document).ready(function () {
 
+    getBookAndDrawItems();
+});
+
+function getBookAndDrawItems() {
     $.ajax({
         type: "GET",
         dataType: "json",
@@ -55,4 +92,5 @@ $(document).ready(function () {
 
         }
     });
-});
+
+}
