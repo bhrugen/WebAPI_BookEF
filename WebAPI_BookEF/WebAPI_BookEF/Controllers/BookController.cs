0636d6dd478a71f13525dedc086d43efce5d2498
Ben Spark <bhrugen1990@gmail.com>
2017-12-12 19:57:11 -0600

    PUT, POST and DELETE HTTP Request

--- WebAPI_BookEF/WebAPI_BookEF/Controllers/BookController.cs
+++ WebAPI_BookEF/WebAPI_BookEF/Controllers/BookController.cs
@@ -36,6 +36,61 @@ namespace WebAPI_BookEF.Controllers
             return Ok(book);
         }
 
+        //PUT : api/Book/5
+        public IHttpActionResult PutItem(int id, Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            Book bookFromDb = db.Books.Find(id);
+
+            if (bookFromDb == null)
+            {
+                return NotFound();
+            }
+
+            bookFromDb.Name = book.Name;
+
+            db.SaveChanges();
+
+            return Ok(db.Books.ToList());
+        }
+
+
+        // POST : api/Book
+        public IHttpActionResult PostBook(Book book)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            db.Books.Add(book);
+            db.SaveChanges();
+
+            return Ok(db.Books.ToList());
+        }
+
+
+        //DELETE : api/Book/5
+        public IHttpActionResult DeleteBook(int id)
+        {
+            Book book = db.Books.Find(id);
+
+            if (book == null)
+            {
+                return NotFound();
+            }
+
+            db.Books.Remove(book);
+            db.SaveChanges();
+
+            return Ok(db.Books.ToList());
+
+        }
+
         protected override void Dispose(bool disposing)
         {
             if (disposing)
